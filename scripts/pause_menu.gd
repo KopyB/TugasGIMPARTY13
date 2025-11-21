e536ad85e6dd05b84a90a526ab74fbbbd160042c
dam27 <damarlegosm27@gmail.com>
2025-11-21 15:48:22 +0700

    Revert "Merge branch 'wth'"
    This reverts commit 4aa6b2f15518e9ae130f4d967510fbb5035e2390, reversing
    changes made to 4326f9d3631db730b48ea81e48c9bf458764e8f1.

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -2,9 +2,7 @@ extends Control
 
 @export var state: Label
 @export var resume_button: Button
-@onready var scorelabel: Label = $VBoxContainer/scorelabel
-@onready var timer: Timer = $Timer
-var score: int = 0
+
 #signal end_screen_toggled(type: int)
 
 const MAP_TYPE_STRING = {0: "YOU DIED!", 1: "PAUSED"}
@@ -19,8 +17,6 @@ func resume():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _ready() -> void:
-	timer.timeout.connect(_on_score_timer_timeout)
-	update_score_display()
 	hide()
 
 func _on_resume_pressed() -> void:
@@ -38,7 +34,7 @@ func _on_exit_pressed() -> void:
 	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("escape") and not get_tree().paused:
-		toggled_handler(0) #masukin var baru buat klo mati atau tidak
+		toggled_handler(1) #masukin var baru buat klo mati atau tidak
 		paused()
 	elif Input.is_action_just_pressed("escape") and get_tree().paused:
 		resume()
@@ -50,19 +46,5 @@ func toggled_handler(type: int) -> void:
 	state.text = MAP_TYPE_STRING[type]
 	if type == 0:
 		resume_button.hide()
-		scorelabel.show()
 	else:
 		resume_button.show()
-		scorelabel.hide()
-		
-func _on_score_timer_timeout():
-	score += 1
-	update_score_display()
-
-func update_score_display():
-	scorelabel.text = "Score: " + str(score)
-
-	# You can add other functions to increase score from game events if needed
-func increase_score(amount: int):
-	score += amount
-	update_score_display()
