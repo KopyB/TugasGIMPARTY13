737c05bd1911ce0010725b4b9e42363474cdd0a2
Herrscher Erlkonig-Kaiser INAN <anonmakantahubulat@gmail.com>
2025-11-22 20:22:33 +0700

    reimplemented gunboat formation and rbomber spawn

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -89,25 +89,47 @@ func spawn_single_obstacle(viewport_rect):
 # --- TIPE 2: GUNBOAT BERKELOMPOK ---
 func spawn_gunboat_group(viewport_rect):
 	var group_count = randi_range(1, 3) # 1 sampai 3 kapal
-	var center_x = randf_range(100, viewport_rect.x - 100)
 	
 	for i in range(group_count):
 		var new_enemy = enemy_scene.instantiate()
 		new_enemy.enemy_type = 0 # Gunboat
+		var enemyshape = new_enemy.get_node("Sprite2D")
 		
-		# Hitung offset agar berjejer rapi
-		var offset_x = (i - (group_count - 1) / 2.0) * 60
+		# Atur formasi berjejer (jarak antar kapal random dari 60 sampai lebar viewport - ukuran sprite/2)
+		var viewport_width = get_viewport().get_visible_rect().size.x - enemyshape.get_rect().size.x/2
+		#aku ganti logika spacingnya biar g fix 60 -kaiser
+		var min_spacing = 60 #minimal 60 kyk kode awal
+		var max_spacing = viewport_width / float(group_count - 1) #ngitung spacing paling jauh berdasarkan lebar viewport sm jumlah kapal
+		var extra_spacing = randi_range(0, max_spacing - min_spacing) #ngitung jarak tambahan
+		var spacing = min_spacing + extra_spacing #rumus jarak antar kapal yg baru
+		# Tentukan posisi tengah grup
+		var half_width = ((group_count - 1) / 2.0) * spacing #aku tambah var ini biar milih titik tengah yang kanan kirinya keluar viewport -kaiser
+		var center_x = randf_range(half_width, viewport_width - half_width) #rumus baru center -kaiser
+
+		var offset_x = (i - (group_count - 1) / 2.0) * spacing
 		
 		new_enemy.global_position = Vector2(center_x + offset_x, -60)
 		get_tree().current_scene.add_child(new_enemy)
 
 # --- TIPE 3: BOMBER DARI SAMPING ---
 func spawn_bomber(viewport_rect):
-	var new_enemy = enemy_scene.instantiate()
-	new_enemy.enemy_type = 1 # Bomber
-	
-	var spawn_x = -60 # Di kiri layar
-	var spawn_y = randf_range(50, viewport_rect.y / 2) # Y acak (setengah atas layar)
-	
-	new_enemy.global_position = Vector2(spawn_x, spawn_y)
-	get_tree().current_scene.add_child(new_enemy)
+	if randi() % 2 == 0:
+		# --- LBOMBER (Spawn Sendiri dari Kiri) ---
+		var new_enemy = enemy_scene.instantiate()
+		new_enemy.enemy_type = 1 # LBomber
+		
+		var spawn_x = -60
+		var spawn_y = randf_range(50, viewport_rect.y / 2)
+		
+		new_enemy.global_position = Vector2(spawn_x, spawn_y)
+		get_tree().current_scene.add_child(new_enemy)
+	else:
+		# --- RBOMBER (Spawn Sendiri dari Kanan) ---
+		var new_enemy = enemy_scene.instantiate()
+		new_enemy.enemy_type = 2 # RBomber
+		
+		var spawn_x = viewport_rect.x + 60
+		var spawn_y = randf_range(50, viewport_rect.y / 2)
+		
+		new_enemy.global_position = Vector2(spawn_x, spawn_y)
+		get_tree().current_scene.add_child(new_enemy)
