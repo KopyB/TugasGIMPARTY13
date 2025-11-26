d9ad2ea3b5e4d07db42b02f0e8fa2d369c9e7c20
dam27 <damarlegosm27@gmail.com>
2025-11-26 22:05:24 +0700

    added powerup notif and icon, tweak score

---
+++ scripts/main.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Powerupview.stop_timer_score()
+	Powerupview.start_timer_score()# Replace with function body.
