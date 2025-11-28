2cb40824dc439a38a685d1c4d0ff11023517e2ac
Luis <brahmanaluismatthew@gmail.com>
2025-11-28 21:01:25 +0700

    Refactor enemy bullet movement and collision handling
    Refactor enemy bullet logic and improve comments

--- scripts/enemybullet.gd
+++ scripts/enemybullet.gd
@@ -1,25 +1,23 @@
 extends Area2D
 
 var speed = 800
-var direction = Vector2.ZERO # Variabel untuk menyimpan arah
+var direction = Vector2.ZERO 
 var explosion_scene = preload("res://scenes/explosion.tscn")
 
 func _ready():
 	add_to_group("enemy_projectiles")
 	
 func _process(delta):
-	# Bergerak sesuai arah yang sudah ditentukan (direction)
 	position += direction * speed * delta
 
 func _on_body_entered(body):
 	print("Peluru Musuh nabrak: ", body.name)
-	# Cek collision (kita perbaiki logika deteksinya di poin 2 nanti)
+	# Cek collision 
 	if body.has_method("take_damage_player"):
 		
 		body.take_damage_player()
 		queue_free()
-	else: # Backup check
-		#queue_free()
+	else: 
 		pass
 
 func meledak():
